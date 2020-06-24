3dec5b27079dc99aca389e65064b42515ed8e456
Woonjang Ahn <woonjangahn@gmail.com>
2020-06-24 16:20:20 +0900

    etc: setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="todoapp">
+      <div>
+        <h1>TODOS</h1>
+        <input
+          id="new-todo-title"
+          className="new-todo"
+          placeholder="할일을 추가해주세요"
+          autofocus
+        />
+      </div>
+      <div className="main">
+        <input className="toggle-all" type="checkbox" />
+        <ul id="todo-list" className="todo-list">
+          <li>
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label className="label">새로운 타이틀</label>
+              <button className="destroy"></button>
+            </div>
+            <input className="edit" value="새로운 타이틀" />
+          </li>
+          <li className="editing">
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label className="label">완료된 타이틀</label>
+              <button className="destroy"></button>
+            </div>
+            <input className="edit" value="완료된 타이틀" />
+          </li>
+          <li className="completed">
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label className="label">완료된 타이틀</label>
+              <button className="destroy"></button>
+            </div>
+            <input className="edit" value="완료된 타이틀" />
+          </li>
+        </ul>
+      </div>
+      <div className="count-container">
+        <span className="todo-count">총 <strong>0</strong> 개</span>
+        <ul className="filters">
+          <li>
+            <a className="all selected" href="#/">전체보기</a>
+          </li>
+          <li>
+            <a className="active" href="#/active">해야할 일</a>
+          </li>
+          <li>
+            <a className="completed" href="#/completed">완료한 일</a>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
